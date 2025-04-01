a8ee715a7112de83877165ef5bd0d621055897d3
Chi Fujii <chi.fujii@ferretdb.io>
2025-04-01 11:48:26 +0900

    run tests in docker

--- run.py
+++ run.py
@@ -9,24 +9,26 @@ from pymongo.errors import ConnectionFailure, OperationFailure
 
 # Constants
 VERSIONS = ["5", "7", "8"]
+LOG_DIRECTORY = "logs"
 LOG_FILENAME = "failed.log"
-SUMMARY_FILENAME = "summary.txt"
 SKIP_DIRECTORY = "skip"
 COMMON_SKIP_FILE = "common.txt"
+LOG_FILE_PATH = os.path.join(LOG_DIRECTORY, LOG_FILENAME)
 COMMON_LIST = os.path.join(SKIP_DIRECTORY, COMMON_SKIP_FILE)
 
-# Default values are used if environment variables are unset
+# User Variables (Consider moving these to a config file or environment variables)
 # Note: No username/password required for MaxScale test
 MONGO_HOST = os.environ.get("MONGO_HOST", "127.0.0.1")
 MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
 MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "admin")
 MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "password")
-AUTHENTICATION_DATABASE = os.environ.get("AUTHENTICATION_DATABASE", "admin")
-AUTHENTICATION_MECHANISM = os.environ.get("AUTHENTICATION_MECHANISM", "SCRAM-SHA-1")
-USE_SSL = os.environ.get("USE_SSL", "false")
-LOAD_BALANCE = os.environ.get("LOAD_BALANCE", "false")
+AUTHENTICATION_DATABASE = "admin"
+AUTHENTICATION_MECHANISM = "SCRAM-SHA-1"
+USE_SSL = "false"
+LOAD_BALANCE = "false"
 TEST_DIRECTORY = os.environ.get("TEST_DIRECTORY", os.path.join(os.getcwd(), "mongo/jstests"))
-LOG_DIRECTORY = os.environ.get("LOG_DIRECTORY", "logs")
+EXEC_DIRECTORY = os.environ.get("EXEC_DIRECTORY", TEST_DIRECTORY) # if docker is used, set this to the scripts directory in the container
+MONGO_COMMAND = os.environ.get("MONGO_COMMAND", "mongo")
 
 if MONGO_USERNAME and MONGO_PASSWORD:
     creds = f"{MONGO_USERNAME}:{MONGO_PASSWORD}@"
@@ -49,17 +51,18 @@ def check_list_existence(file_path, file_description):
 
 # Validate MongoDB connectivity and credentials.
 def validate_connection(uri):
+    ping_command = MONGO_COMMAND.split() + ["--eval", "db.runCommand({ping: 1})", uri]
     try:
-        client = MongoClient(uri, serverSelectionTimeoutMS=5000)
-        client[AUTHENTICATION_DATABASE].command("ping")
+        subprocess.run(
+            ping_command,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            timeout=5,
+        )
         print("Connected successfully to database")
-
-    except ConnectionFailure:
+    except (subprocess.CalledProcessError, subprocess.TimeoutExpired):
         print("\033[91mFailed to connect to database\033[0m")
-        sys.exit(1)
-
-    except OperationFailure:
-        print("\033[91mAuthentication failed for database\033[0m")
         sys.exit(1)
 
 
@@ -70,13 +73,14 @@ def create_mongo_uri():
 
 # Build the mongo command with the necessary arguments.
 def build_mongo_command(script_path):
-    mongo_command = ["mongo", f"mongodb://{creds}{MONGO_HOST}:{MONGO_PORT}/{auth_part}"]
+    mongo_command = MONGO_COMMAND.split() + [f"mongodb://{creds}{MONGO_HOST}:{MONGO_PORT}/{auth_part}"]
     mongo_command.append(script_path)
     return mongo_command
 
 
 # Run an individual script using subprocess.
-def run_script(script_path):
+def run_script(test_directory, script_path):
+    script_path = os.path.join(test_directory, script_path)
     mongo_command = build_mongo_command(script_path)
     # print(mongo_command)
     try:
@@ -92,25 +96,26 @@ def run_script(script_path):
         return False
 
 
-# Find all JS files that are not in the exclusion lists.
+# Find all JS files that are not in the exclusion lists relative to the given directory.
 def find_js_files(directory, mongo_version):
     exclusions = set()
     with open(COMMON_LIST, "r") as f:
-        exclusions.update([TEST_DIRECTORY + line.strip() for line in f])
+        exclusions.update([line.strip() for line in f])
 
     # Add specific mongo version exclusions.
     mongo_exclusions = f"{SKIP_DIRECTORY}/mongo{mongo_version}.txt"
     check_list_existence(mongo_exclusions, mongo_exclusions)
     with open(mongo_exclusions, "r") as f:
-        exclusions.update([TEST_DIRECTORY + line.strip() for line in f])
+        exclusions.update([line.strip() for line in f])
 
     js_files = []
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".js"):
                 full_path = os.path.join(root, file)
-                if full_path not in exclusions:
-                    js_files.append(full_path)
+                relative_path = os.path.relpath(full_path, directory)
+                if os.path.join("/", relative_path) not in exclusions:
+                    js_files.append(relative_path)
 
     if not js_files:
         print(f"No JS files found in {directory}")
@@ -144,7 +149,7 @@ if __name__ == "__main__":
         filename = os.path.basename(script_path)
 
         script_start = time.time()  # Start time for the current script
-        success = run_script(script_path)
+        success = run_script(EXEC_DIRECTORY, script_path)
         script_end = time.time()  # End time for the current script
 
         # Calculate elapsed time for this script
@@ -176,15 +181,9 @@ if __name__ == "__main__":
     )
     print(summary_log)
 
-    summary_file_path = os.path.join(LOG_DIRECTORY, SUMMARY_FILENAME)
-    with open(summary_file_path, "w") as summary_file:
-        summary_file.write(summary_log)
-        print(f"Summary have been written to: {summary_file_path}")
-
-    log_file_path = os.path.join(LOG_DIRECTORY, LOG_FILENAME)
     if failed_scripts:
-        with open(log_file_path, "w") as log_file:
+        with open(LOG_FILE_PATH, "w") as log_file:
             for failed_script in failed_scripts:
                 relative_path = os.path.relpath(failed_script, TEST_DIRECTORY)
                 log_file.write(f"{relative_path}\n")
-        print(f"Failed scripts have been logged to: {log_file_path}")
+        print(f"Failed scripts have been logged to: {LOG_FILE_PATH}")
